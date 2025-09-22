843c5d18878c4e83f2c9f2847fe6c141d6fb1f6d
GR <immemo1105@yahoo.com>
2025-09-21 21:58:08 -0700

    Home Page

---
+++ tacomatrivia-frontend/src/features/UI/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Hero from "../components/Hero.jsx"
+import JoinModCTA from "../components/BecomeModCTA.jsx"
+export default function Home(){
+    return(
+        <div>
+            <Hero />
+            <JoinModCTA />
+        </div>
+    )
+}
