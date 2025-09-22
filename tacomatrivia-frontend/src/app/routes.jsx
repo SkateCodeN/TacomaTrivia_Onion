ff08dd944c3692e45f7e792af65453b49086dfcf
GR <immemo1105@yahoo.com>
2025-09-21 21:47:44 -0700

    Our home route now has a home element.

--- tacomatrivia-frontend/src/app/routes.jsx
+++ tacomatrivia-frontend/src/app/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from '@widgets/Layout.jsx';
+import Home from '@features/UI/pages/Home.jsx'
 
 const VenuesListPage = lazy(() => import('@features/venues/pages/VenuesListPage.jsx'));
 const NotFound = () => <div style={{ padding: 24 }}>Not found</div>;
@@ -9,7 +10,7 @@ export const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { index: true, element: <VenuesListPage /> },  // '/'
+      { index: true, element: <Home /> },  // '/'
       { path: 'venues', element: <VenuesListPage /> }
     ],
   },
