0aaef596c43f261afa9ae15dc1c412747b271ccb
GR <immemo1105@yahoo.com>
2025-09-25 23:12:46 -0700

    Domain Layer TacomaVenue now has the same properties that reflect the postgres (real) DB

--- TacomaTrivia.Domain/TacomaVenue.cs
+++ TacomaTrivia.Domain/TacomaVenue.cs
@@ -12,17 +12,68 @@ public sealed class TacomaVenue
     public bool AllowsPets { get; private set; }
     public int Rounds { get; private set; }
 
-    public static TacomaVenue Create(string name, string? phone, string? address, bool allowsPets, int rounds)
+    public int TriviaDay { get; private set; }
+    public DateTime TriviaStart { get; private set; }
+
+    public string? Website { get; private set; }
+
+    public bool AllowsKids{ get; private set; }
+
+
+    public static TacomaVenue Create
+    (
+        string name,
+        string? phone,
+        string? address,
+        bool allowsPets,
+        int rounds,
+        int triviaDay,
+        DateTime triviaStart,
+        string? website,
+        bool allowsKids
+    )
     {
         if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name required");
         if (rounds < 0) throw new ArgumentOutOfRangeException(nameof(rounds));
-        return new TacomaVenue { Name = name.Trim(), Phone = phone, Address = address, AllowsPets = allowsPets, Rounds = rounds };
+        if (triviaDay < 0 && triviaDay > 7) throw new ArgumentOutOfRangeException(nameof(triviaDay));
+        return new TacomaVenue
+        {
+            Name = name.Trim(),
+            Phone = phone,
+            Address = address,
+            AllowsPets = allowsPets,
+            Rounds = rounds,
+            TriviaDay = triviaDay,
+            TriviaStart = triviaStart,
+            Website = website,
+            AllowsKids = allowsKids
+        };
     }
 
-    public void Update(string name, string? phone, string? address, bool allowsPets, int rounds)
+    public void Update
+    (
+        string name,
+        string? phone,
+        string? address,
+        bool allowsPets,
+        int rounds,
+        int triviaDay,
+        DateTime triviaStart,
+        string? website,
+        bool allowsKids
+    )
     {
         if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name required");
         if (rounds < 0) throw new ArgumentOutOfRangeException(nameof(rounds));
-        Name = name.Trim(); Phone = phone; Address = address; AllowsPets = allowsPets; Rounds = rounds;
+        if (triviaDay < 0 && triviaDay > 7) throw new ArgumentOutOfRangeException(nameof(triviaDay));
+        Name = name.Trim();
+        Phone = phone;
+        Address = address;
+        AllowsPets = allowsPets;
+        Rounds = rounds;
+        TriviaDay = triviaDay;
+        TriviaStart = triviaStart;
+        Website = website;
+        AllowsKids = allowsKids;
     }
 }
