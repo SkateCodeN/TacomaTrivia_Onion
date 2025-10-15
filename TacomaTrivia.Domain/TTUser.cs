3eb044d119f3a747d894f34fc2da90d3757c9d27
GR <immemo1105@yahoo.com>
2025-10-14 19:22:31 -0700

    We create a TacomaTrivia Aggregate to be for our users that will be allowed to use the application

---
+++ TacomaTrivia.Domain/TTUser.cs
@@ -0,0 +1,52 @@
+namespace TacomaTrivia.Domain;
+
+/// <summary>
+/// Domain entity. Invariants live here.
+/// </summary>
+public sealed class TTUser
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; private set; } = default!;
+    // For MFA?
+    public string? Phone { get; private set; }
+    // For Auth and Authorization
+    public string? Email { get; private set; }
+    
+    public string? Role { get; private set; }
+
+
+    public static TTUser Create
+    (
+        string name,
+        string? phone,
+        string? email,
+        string? role
+    )
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name required");
+
+        return new TTUser
+        {
+            Name = name.Trim(),
+            Phone = phone,
+            Email = email,
+            Role = role
+        };
+    }
+
+
+    public void Update(
+        string name,
+        string? phone,
+        string? email,
+        string? role
+    )
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name required");
+
+        Name = name.Trim();
+        Phone = phone;
+        Email = email;
+        Role = role;
+    }
+}
