14fed5e1ac4aa82c316026439c6b88ff8ec2d7fa
GR <immemo1105@yahoo.com>
2025-10-20 19:29:10 -0700

    Routes, specifically the users route serves us the table

--- tacomatrivia-frontend/src/app/routes.jsx
+++ tacomatrivia-frontend/src/app/routes.jsx
@@ -4,6 +4,8 @@ import Layout from '@widgets/Layout.jsx';
 import Home from '@features/UI/pages/Home.jsx'
 
 const VenuesListPage = lazy(() => import('@features/venues/pages/VenuesListPage.jsx'));
+
+const UsersListPage = lazy( () => import ('@features/users/components/UsersTable'))
 const NotFound = () => <div style={{ padding: 24 }}>Not found</div>;
 
 export const router = createBrowserRouter([
@@ -11,7 +13,8 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },  // '/'
-      { path: 'venues', element: <VenuesListPage /> }
+      { path: 'venues', element: <VenuesListPage /> },
+      { path: 'users', element: <UsersListPage /> }
     ],
   },
   { path: '*', element: <NotFound /> }
