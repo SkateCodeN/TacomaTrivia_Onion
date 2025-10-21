ded025fe466c12f38fe282107fd228e25d0f478e
GR <immemo1105@yahoo.com>
2025-10-20 19:34:25 -0700

    Users Api that calls the http (shared) calls, does have some pay attention to Create, edit and delete as they have specific returns

---
+++ tacomatrivia-frontend/src/features/users/api/usersApi.js
@@ -0,0 +1,29 @@
+import { http } from '@shared/api/http';
+
+// Server accepts: q, page, pageSize
+// Response may be either an array OR { items, total } depending on your implementation.
+export const usersApi = {
+  list: async (params) => {
+    const data = await http.get('/users', params);
+    if (Array.isArray(data)) return { items: data, total: undefined };
+    return { items: data.items ?? [], total: data.total };
+  },
+  create: async(body) => {
+    const {headers,data} = await http.post('/users', body);
+    const loc = headers?.location;
+    const id = loc?.split('/').pop();
+    //console.log('Created id:', id);
+    
+  },
+  edit: async(id,body) => {
+    const {status} = await http.put(`/users/${id}`, body);
+    
+    console.log('Edited User, status:', status);
+    return status;
+  },
+  delete: async(id) => {
+    const {status} = await http.del(`/users/${id}`);
+    //console.log('User was deleted, here is the status:', status)
+    return status;
+  }
+};
