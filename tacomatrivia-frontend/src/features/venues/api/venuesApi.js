39ca96beadfdbf8caec161e8d2deb2e97a3bc2f9
GR <immemo1105@yahoo.com>
2025-11-04 16:17:40 -0800

    In the Venues API we created the functions to call Create, Edit and DELETE of our FRONTEND CRUD

--- tacomatrivia-frontend/src/features/venues/api/venuesApi.js
+++ tacomatrivia-frontend/src/features/venues/api/venuesApi.js
@@ -8,4 +8,22 @@ export const venuesApi = {
     if (Array.isArray(data)) return { items: data, total: undefined };
     return { items: data.items ?? [], total: data.total };
   },
+  create: async(body) => {
+    const {headers,data} = await http.post('/venues', body);
+    const loc = headers?.location;
+    const id = loc?.split('/').pop();
+    //console.log('Created id:', id);
+    
+  },
+  edit: async(id,body) => {
+    const {status} = await http.put(`/venues/${id}`, body);
+    
+    console.log('Edited venues, status:', status);
+    return status;
+  },
+  delete: async(id) => {
+    const {status} = await http.del(`/venues/${id}`);
+    //console.log('User was deleted, here is the status:', status)
+    return status;
+  }
 };
