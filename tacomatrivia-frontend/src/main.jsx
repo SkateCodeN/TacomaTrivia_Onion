3771ac939cc324a213b28fa8a865785e543c2f0c
GR <immemo1105@yahoo.com>
2025-12-11 20:14:04 -0800

    we cleaned main to not display sensitive info but we wrap app component in the Auth0 Parent component

--- tacomatrivia-frontend/src/main.jsx
+++ tacomatrivia-frontend/src/main.jsx
@@ -6,8 +6,22 @@ import App from './app/App.jsx';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 
+// we installed Auth0 library
+import { Auth0Provider } from '@auth0/auth0-react';
+//console.log("this is the origin page", window.location.origin )
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Auth0Provider
+  domain={""}
+  clientId={""}
+  authorizationParams={{
+    redirect_uri: window.location.origin + "/auth/callback",
+    audience:"https://tacomatrivia.com/authorizeUser",
+    scope: "openid profile email read:trivia write:trivia"
+  }}
+  
+  >
+    
     <App />
-  </StrictMode>
+  
+  </Auth0Provider>
 );
