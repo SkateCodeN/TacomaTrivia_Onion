26967b88e36cc84bfed8d29302aea9d8afdf788f
GR <immemo1105@yahoo.com>
2026-02-25 17:57:52 -0800

    reconfigure .net backend port

--- tacomatrivia-frontend/vite.config.js
+++ tacomatrivia-frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Frontend calls /api/* → forwarded to http://localhost:5141/api/*
       '/api': {
-        target: 'http://localhost:5141',
+        target: 'http://localhost:5067',
         changeOrigin: true,
         secure: false,
       },
