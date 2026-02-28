6159db6b8a91d1d502b95e57d62164d79aa9b22e
GR <immemo1105@yahoo.com>
2026-02-27 16:41:21 -0800

    We add the component that will handle auth in the nav

---
+++ tacomatrivia-frontend/src/components/NavAuthComp.jsx
@@ -0,0 +1,12 @@
+
+import { Group,Button } from '@mantine/core'
+export default function NavAuthComp() {
+
+    return (
+        <Group visibleFrom="sm">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+        </Group>
+
+    )
+}
