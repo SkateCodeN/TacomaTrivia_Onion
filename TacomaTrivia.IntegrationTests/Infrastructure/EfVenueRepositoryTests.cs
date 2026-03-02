4b8415c6830d5c31db9a7caabff4dddf815294bc
GR <immemo1105@yahoo.com>
2026-03-02 15:47:22 -0800

    FIxed Domain Level Integration Tests

--- TacomaTrivia.IntegrationTests/Infrastructure/EfVenueRepositoryTests.cs
+++ TacomaTrivia.IntegrationTests/Infrastructure/EfVenueRepositoryTests.cs
@@ -14,12 +14,12 @@ public class EfVenueRepositoryTests : PostgresFixture
     {
         var repo = new EfVenueRepository(NewContext());
 
-        var v = TacomaVenue.Create("Alma Mater","253-555-1234","1322 Fawcett Ave", true, 5);
+        var v = TacomaVenue.Create("Alma Mater","253-555-1234","1322 Fawcett Ave", true, 5, 1,new TimeOnly(14, 30), "www.test.com", true);
         var id = await repo.AddAsync(v, default);
 
         (await repo.GetByIdAsync(id, default))!.Name.Should().Be("Alma Mater");
 
-        v.Update("New Name","2","B", false, 6);
+        v.Update("New Name","2","B", false, 6, 6, new TimeOnly(17,30),"www.apothecary.com", false);
         await repo.UpdateAsync(v, default);
 
         (await repo.GetByIdAsync(id, default))!.Name.Should().Be("New Name");
@@ -32,8 +32,8 @@ public class EfVenueRepositoryTests : PostgresFixture
     public async Task Search_Uses_Ilike_Substring()
     {
         var repo = new EfVenueRepository(NewContext());
-        await repo.AddAsync(TacomaVenue.Create("Alma Mater","p","a", true, 5), default);
-        await repo.AddAsync(TacomaVenue.Create("Tacoma Taproom","p","a", false, 3), default);
+        await repo.AddAsync(TacomaVenue.Create("Alma Mater","p","a", true, 5, 4, new TimeOnly(11,00), "ww", true), default);
+        await repo.AddAsync(TacomaVenue.Create("Tacoma Taproom","p","a", false, 3,4, new TimeOnly(11,00), "ww", true), default);
 
         var hits = await repo.SearchAsync("ALMA", 1, 10, default);
         hits.Select(x => x.Name).Should().Contain("Alma Mater");
