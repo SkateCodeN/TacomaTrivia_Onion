3aabecb54bcffb08d9a5d7994db091e7ed5d1979
G <immemo1105@yahoo.com>
2026-03-11 12:45:33 -0700

    featured event gets the current date and seperates the day. Then we reach out to the backend to get a filtered list of trivia happening on that day.

--- tacomatrivia-frontend/src/components/FeaturedEvents.jsx
+++ tacomatrivia-frontend/src/components/FeaturedEvents.jsx
@@ -1,18 +1,46 @@
 // src/components/FeaturedEvents.jsx
 import { Title, SimpleGrid } from '@mantine/core';
+import { useEffect,useState } from 'react';
 import EventCard from './EventCard';
-
+import axios from 'axios';
+import DayConverter from '@features/venues/helper functions/DayConverter';
 export default function FeaturedEvents() {
+  const [venues, setVenues] = useState([]);
+  const [day, setDay] = useState("");
+  
+  //Get the date for today
+  const getTodayDate = () =>{
+    const currentDate = new Date(); 
+    
+    const dayOfWeekNumber = currentDate.getDay();
+    return dayOfWeekNumber;
+  }
+
+  
+
+  const  getCurrentTriviabyDay = async () =>
+  {
+    const currentDay = getTodayDate();
+    setDay(currentDay);
+    const response = await axios.get(`api/venues/${currentDay}`);
+    setVenues(response.data);
+  }
+  useEffect(() =>{
+    getCurrentTriviabyDay();
+  },[])
+  
   return (
     <>
       <Title order={2} mb="lg">
-        Upcoming Trivia Events (working on this...)
+       {DayConverter(day)} Trivia
       </Title>
 
       <SimpleGrid cols={{ base: 1, sm: 2, md: 3 }} spacing="lg">
-        <EventCard />
-        <EventCard />
-        <EventCard />
+       {
+        venues.map( venue => (
+          <EventCard key={venue.id} venueData={venue} day={day} />
+        ))
+       }
       </SimpleGrid>
     </>
   );
