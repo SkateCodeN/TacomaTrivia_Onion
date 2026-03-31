8e7bff9ebeea3018f37b0bd3752a4e645d46441e
GR <immemo1105@yahoo.com>
2026-03-31 10:35:55 -0700

    Swagger and Auth + Authorization pipeline added to middleware

--- TacomaTrivia.Api/Program.cs
+++ TacomaTrivia.Api/Program.cs
@@ -4,6 +4,12 @@ using TacomaTrivia.Application.Services;
 using TacomaTrivia.Application.Contracts;
 using TacomaTrivia.Infrastructure;
 using TacomaTrivia.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
+using TacomaTrivia.Api.Auth;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +30,63 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Auth Middleware
+
+// 1. Get secret key from Config (User Secrets / Env vars -- This is for when it goes to server)
+var jwtKey = builder.Configuration["Jwt:Key"] ?? "Temp";
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+
+// 2. Configure Auth (Id who the user is)
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "tacomatrivia.com",
+        ValidateAudience = true,
+        ValidAudience = "tacomatrivia.com",
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key,
+        ValidateLifetime = true
+    };
+});
+
+// 3. Configure Authorization (Decides what the user can do)
+builder.Services.AddAuthorization(options =>
+{
+    // "ReadOnly" allows both Admin and TestUser to view data
+    options.AddPolicy(AuthConfig.ReadOnlyPolicy, policy =>
+        policy.RequireRole(AuthConfig.AdminRole, AuthConfig.TestUserRole));
+
+    // "AdminOnly" strictly requires the Admin role 
+    options.AddPolicy(AuthConfig.AdminPolicy, policy =>
+        policy.RequireRole(AuthConfig.AdminRole));
+
+});
+
+// We confifure Swagger to handle JWT and to test with it
+
+builder.Services.AddSwaggerGen( config =>
+{
+    config.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Paste JWT Token here"
+    });
+
+    config.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement {
+        { new Microsoft.OpenApi.Models.OpenApiSecurityScheme { 
+            Reference = new Microsoft.OpenApi.Models.OpenApiReference { Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme, Id = "Bearer" }
+        }, Array.Empty<string>() }
+    });
+});
 
 var app = builder.Build();
 
@@ -37,26 +100,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
 
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast = Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
-
+// We register our auth and swagger middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
 // So that we can use our published frontned 
 // we will use the middleware to render static files from wwwroot
 app.UseStaticFiles();
@@ -67,10 +116,6 @@ app.MapFallbackToFile("index.html");
 
 app.Run();
 
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
 
 //created for Api integration tests
 public partial class Program { }
