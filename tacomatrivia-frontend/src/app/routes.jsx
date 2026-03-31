5c338cd4ff756c19b9fe62f7bf44105ae0b89bf1
GR <immemo1105@yahoo.com>
2026-03-31 10:25:40 -0700

    Loginpage and auth user page routes created for react router

--- tacomatrivia-frontend/src/app/routes.jsx
+++ tacomatrivia-frontend/src/app/routes.jsx
@@ -3,6 +3,9 @@ import { lazy } from 'react';
 import Layout from '@widgets/Layout.jsx';
 import Test from './Test';
 import Home from '../../src/pages/Home';
+
+import AuthUser from '@features/Form-Auth/AuthUser';
+import LoginPage from '@features/Form-Auth/LoginPage';
 const VenuesListPage = lazy(() => import('@features/venues/pages/VenuesListPage.jsx'));
 const NotFound = () => <div style={{ padding: 24 }}>Not found</div>;
 
@@ -11,8 +14,10 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },  // '/'
-      { index: true, element: <Home /> },  // '/'
-      { path: 'venues', element: <VenuesListPage /> }
+      { path: 'venues', element: <VenuesListPage /> },
+      {path: 'login', element: <LoginPage />},
+
+      {path:'/auth/user', element: <AuthUser />}
     ],
   },
   { path: '*', element: <NotFound /> }
