b8d9423a6e7b494dc0ad4c6841292ea376be7b6a
G <immemo1105@yahoo.com>
2026-03-31 22:28:31 -0700

    Login Page now has an Auth0 login flow

--- tacomatrivia-frontend/src/features/Form-Auth/LoginPage.jsx
+++ tacomatrivia-frontend/src/features/Form-Auth/LoginPage.jsx
@@ -1,12 +1,25 @@
 import {
-    Container
+    Container,
+    Stack,
+    Button,
+    Title
 } from '@mantine/core';
 import LoginForm from "./LoginForm";
 export default function LoginPage() 
 {
+    const handleClick = () =>{
+        window.location.href ='http://localhost:5067/api/auth0';
+    }
     return(
         <Container  p="lg" size={500}>
             <LoginForm />
+
+            <Stack p="lg">
+                <Title order={3}>Auth0 Test</Title>
+                <Button onClick={handleClick}>Login</Button>
+            </Stack>
         </Container>
+
+        
     );
 }
